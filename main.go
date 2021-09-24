812ef448415b33de7f66b0781b970967ed8843b4
bayubagusbagaswara <@bagaszwara12@gmail.com>
2021-09-24 20:19:07 +0700

    menambahkan go.mod dan main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// cetak angka 1
+	fmt.Println("Satu = ", 1)
+
+	// cetak angka 2
+	fmt.Println("Dua = ", 2)
+
+	// cetak angka 3
+	fmt.Println("Tiga Koma Lima = ", 3.5)
+}
