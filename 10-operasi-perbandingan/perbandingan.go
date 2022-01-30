62f08351e039547b44493225b5dfcb5cc1cc38e8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-01-30 09:03:15 +0700

    Operasi Perbandingan

---
+++ 10-operasi-perbandingan/perbandingan.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name1 := "Bayu"
+	name2 := "Bayu"
+
+	// bandingkan nilai name1 dengan name2
+	result := name1 == name2
+	fmt.Println(result)
+
+	value1 := 100
+	value2 := 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
